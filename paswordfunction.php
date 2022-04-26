9a4413a73d867387862694831ac31751d5c13803
Gabi-Tho <102246449+Gabi-Tho@users.noreply.github.com>
2022-04-26 09:22:29 -0400

    made into function

--- paswordfunction.php
+++ paswordfunction.php
@@ -1,42 +1,34 @@
 <?php
 
-$password = "funhappyzone";
+$password = "zone";
 function validatePassword($password){
-    $password= trim($password);
-    if (strlen($password) != 10 ){
+   $password= trim($password);
+     if (strlen($password) < 10 ){
         echo "password is not long enough<br>";
     }
-    if(strlen($password) != mb_strlen($password)){
-        echo "password contains special characters";
+     if(strlen($password) != mb_strlen($password)){
+        echo "password contains special characters<br>";
+    }
+    if(count(count_chars($password,1)) < 6){
+        echo "password needs more characters<br>";
+    }
+    for ($i = 0; $i < strlen($password); $i++){
+        $c = $password[$i];
+        if($c >= "a" &&  $c <= "z"){
+            echo "fun lowercase time"."<br>";
+            $alphabetLowercase = true;
+        }if($c >= "A" && $c <= "Z" ){
+            echo "FUN UPPERCASE TIME"."<br>";
+            $alphabetUppercase = true;
+        }if($c >= "0" && $c <= "9"){
+            echo "you have a number"."<br>";
+            $number = true;
+        }if($c == "!" || $c == "#" || $c == "$" || $c == "%" || $c == "&" || $c == "*" || $c ==  "+" || $c == "-" || $c == ":" || $c == ";" || $c == "="){  
+            echo "you have a special sign"."<br>";
+            $specialChar= true;  
+        }
     }
 }
 
-echo validatePassword($password);
-
-
-$password = "!abcdABCD1234";
-for ($i = 0; $i < strlen($password); $i++) {
-  $c = $password[$i];
-  if($c >= "a" &&  $c <= "z"){
-      echo "fun lowercase time"."<br>";
-      $alphabetLowercase = true;
-  }
-  if($c >= "A" && $c <= "Z" ){
-      echo "FUN UPPERCASE TIME"."<br>";
-      $alphabetUppercase = true;
-  }
-  if($c >= "0" && $c <= "9"){
-      echo "you have a number"."<br>";
-      $number = true;
-  }
-  if($c == "!" ) || () || ($c == "#" ) || ($c == "$" ) || ($c == "%" ) || ($c == "!" ) || ($c == "!" ) || ($c == "!" ) {
-      echo "you have a special sign"."<br>";
-      $specialChar= true;
-  }
-  if($c == "#" ){
-    echo "you have a special sign"."<br>";
-    $specialChar= true;
-    
-}
-
-//test githubcommit
+validatePassword($password);
+?>
